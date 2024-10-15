e8750963110fe0f6fb87f6416d849a41074916f8
nyllson <100968768+myllsonnp@users.noreply.github.com>
2024-10-15 20:27:25 -0300

    Update index.ts

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -8,44 +8,11 @@ router.get("/resolution", (req, res) => {
 })
 
 
-router.get('/video', (req, res) => {
-  const videoPath = path.join(__dirname, '..','..', 'videos', 'video.mp4');
-  const stat = fs.statSync(videoPath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
-
-  if (range && range.startsWith('bytes=')) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize - 1;
-    const chunksize = (end - start) + 1;
-    console.log(`Start: ${start}, End: ${end} Chunksize: ${chunksize}`);
-    const file = fs.createReadStream(videoPath, { start, end, signal: AbortSignal.timeout(1000) });
-    const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-    };
-    res.writeHead(206, head);
-    file.pipe(res);
-} else {
-    const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(videoPath).pipe(res);
-}
-});
-
 router.get('/fetch', async (req, res) => {
   res.send(`
     <div style="width: 640px; height: 360px">
       <video controls width="100%" height="100%" muted onDoubleClick={() => { alert('Você não pode tocar esse video'); }}>
-        <source src="http://192.168.1.3:3334/video" type="video/mp4" />
+        <source src="http://localahost:3334/video" type="video/mp4" />
       </video>
     </div>
   `);
